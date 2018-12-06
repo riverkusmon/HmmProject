410032d1e9852703ba5e2b75f414661148352c87
Andrew <andrewgardner@live.com>
2018-12-06 13:15:40 -0700

    adde images and functionality

---
+++ HmmProject/Controller/AbstractionViewController.swift
@@ -0,0 +1,96 @@
+//
+//  AbstractionViewController.swift
+//  HmmProject
+//
+//  Created by Gardner, Andrew on 12/6/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class AbstractionViewController: UIPageViewController, UIPageViewControllerDataSource {
+
+   public override func viewDidLoad() {
+        super.viewDidLoad()
+        dataSource = self
+    
+        if let firstViewController = orderedAbstractionViews.first
+        {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
+
+    }
+    
+    private lazy var orderedAbstractionViews : [UIViewController] =
+    {
+        return
+        [
+            self.newAbstractionViewController(abstractionLevel: "swift"),
+            self.newAbstractionViewController(abstractionLevel: "block"),
+            self.newAbstractionViewController(abstractionLevel: "object"),
+            self.newAbstractionViewController(abstractionLevel: "binary"),
+            self.newAbstractionViewController(abstractionLevel: "gate")
+        ]
+    }()
+
+    private func newAbstractionViewController(abstractionLevel : String) -> UIViewController
+    {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "\(abstractionLevel)ViewController")
+    }
+    //left
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController:
+    UIViewController) -> UIViewController?
+    {
+        guard let  viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+            else
+        {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0
+            else
+        {
+            return orderedAbstractionViews.last
+        }
+        
+        guard orderedAbstractionViews.count > previousIndex
+            else
+        {
+            return nil
+        }
+        
+        return orderedAbstractionViews[previousIndex]
+    }
+    
+    //right
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController:
+        UIViewController) -> UIViewController?
+    {
+        guard let  viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+            else
+        {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex + 1
+        
+        guard previousIndex >= 0
+            else
+        {
+            return orderedAbstractionViews.last
+        }
+        
+        guard orderedAbstractionViews.count > previousIndex
+            else
+        {
+            return nil
+        }
+        
+        return orderedAbstractionViews[previousIndex]
+    }
+}
